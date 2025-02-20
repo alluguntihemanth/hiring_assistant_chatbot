e63e884b4a0efc4904fd5eda5711cc87540b45c8
Hemanth Reddy Allugunti <93366548+alluguntihemanth@users.noreply.github.com>
2025-02-20 15:19:55 +0530

    Update gemini_api.py

--- gemini_api.py
+++ gemini_api.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
-load_dotenv()
+#load_dotenv()
 
 # Configure API Key
 GEMINI_API_KEY = st.secrets["gemini"]["api_key"]
